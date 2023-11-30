9213124e08b35c242b3d2091ce7f0b9e601640c8
iRipple <iripple@iRipples-MacBook-Air.local>
2023-11-30 16:06:42 +0800

    test after test

--- src/hooks/apiServices/getToken.js
+++ src/hooks/apiServices/getToken.js
@@ -6,14 +6,14 @@ const encoded = encode(SIGNUP_TOKEN)
 
 export const GetToken = async (account_name, email, password) => {
     console.log(account_name,email,password)
-    console.log('hello')
     return axios({
       method: "get",
-      url:`https://${account_name}.imonggo.net/api/tokens.json?&account_name=${account_name}&email=${email}&password=${password}`,
+      url:`https://${account_name}.imonggo.com/api/tokens.json?&account_name=${account_name}&email=${email}&password=${password}`,
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
-        
+        "User-Agent": "com.imonggo.stockroom",
+        Accept: "application/json",
+          // Authorization: `Basic ` + `${encoded}`,
       },
     });
   };
