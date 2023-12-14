4b9cbf1041c78ed671c6634d62da1bd188b36f0b
iRipple <iripple@iRipples-MacBook-Air.local>
2023-12-14 10:23:04 +0800

    hackaton

--- src/hooks/apiServices/getToken.js
+++ src/hooks/apiServices/getToken.js
@@ -11,8 +11,10 @@ export const GetToken = async (account_name, email, password) => {
       url:`https://${account_name}.imonggo.net/api/tokens.json?&account_name=${account_name}&email=${email}&password=${password}`,
       headers: {
         "Content-Type": "application/json",
+        "Allow-Control-Allow-Origin": "",
         "User-Agent": "com.imonggo.stockroom",
         Accept: "application/json",
+        
           // Authorization: `Basic ` + `${encoded}`,
       },
     });
